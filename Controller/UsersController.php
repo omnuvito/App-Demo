fd09eaf7494bde6a0c4d4363ac41d0da07dba512
José Alejandro Álvarez Mendoza <alvarez.alejandro3@gmail.com>
2014-09-12 11:14:43 -0430

    Login updated

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -18,7 +18,14 @@ class UsersController extends AppController {
 
 	public function beforeFilter()
 	{
-		$this->Auth->allow('add','login');
+		parent::beforeFilter();
+		$this->Cookie->name = 'app-demo-id';
+		$this->Cookie->time = '3600';
+		$this->Cookie->secure = 'true';
+		$this->Cookie->key = '37>]3%$W4FUYgf$>33hxrc{mFM/g7ri[Fkofcsa]4sEgx8Tdnjvy4wT@n';
+    	$this->Cookie->httpOnly = true;
+    	$this->Cookie->type('aes');
+		$this->Auth->allow('add','logout');
 	}
 
 /**
@@ -56,7 +63,7 @@ class UsersController extends AppController {
 			$this->User->create();
 			if ($this->User->save($this->request->data)) {
 				$this->Session->setFlash(__('The user has been saved.'));
-				return $this->redirect(array('action' => 'view'));
+				return $this->redirect(array('controller'=>'posts','action' => 'view'));
 			} else {
 				$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
 			}
@@ -105,6 +112,7 @@ class UsersController extends AppController {
 		}
 		$this->request->allowMethod('post', 'delete');
 		if ($this->User->delete()) {
+			$this->Session->destroy();
 			$this->Session->setFlash(__('The user has been deleted.'));
 		} else {
 			$this->Session->setFlash(__('The user could not be deleted. Please, try again.'));
@@ -115,11 +123,14 @@ class UsersController extends AppController {
 	public function login()
 	{
 		if ($this->request->is('post')) {
-	        if ($this->Auth->login($this->request->data)){
-	        	$userSession = $this->sessionData($this->request->data['User']['username']);
-	        	CakeSession::write('User', $userSession['User']);
-	        	$user = $this->User->find('first', array('conditions' => array('User.username = ' => $this->request->data['User']['username'])));
-	            return $this->redirect(array('action'=>'view', $user['User']['id']));
+	        if ($this->Auth->login()) {
+	        	//Buscamos informacion del usuario con el nombre del usuario.
+	        	$userCookie = $this->sessionData($this->request->data['User']['username']);
+	        	//Guardamos en la variable de sesion la informacion del usuario.
+	        	$this->Cookie->write('User', $userCookie['User'],true);
+	        	// //Buscamos el usuario correspondiente al nombre de usuario.
+	        	// $user = $this->User->find('first', array('conditions' => array('User.username = ' => $this->request->data['User']['username'])));
+	            return $this->redirect($this->Auth->redirect());
 	        }
 	        $this->Session->setFlash(__('Invalid username or password, try again'));
 	    }
@@ -127,12 +138,19 @@ class UsersController extends AppController {
 
 	public function logout()
 	{
-		CakeSession::delete('User');
-		return $this->redirect('/');
+		//Destruimos la sesion del usuario.
+		$this->Cookie->destroy('User');
+		return $this->redirect($this->Auth->logout());
 	}
 
+/*
+*	sessionData method
+*	Metodo para buscar informacion del usuario que va a ser guardada en sesion
+*	@param string $username
+*	@return object
+*/
 	public function sessionData($username){
-		$user = $this->User->find('first', array('condition' => array('User.username = '=>$username)));
+		$user = $this->User->find('first', array('condition' => array('User.username = '.$username)));
 		return $user;
 	}
 }
